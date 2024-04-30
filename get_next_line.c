53813b4ab5fc77e784c5f8e9935952832b84bc1d
Letícia Aguiar <leticia_silva@trimble.com>
2024-04-30 10:13:00 -0300

    feat: initial commit

---
+++ get_next_line.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letsaguiar <letsaguiar@yandex.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 10:00:44 by letsaguiar        #+#    #+#             */
+/*   Updated: 2024/04/30 10:11:59 by letsaguiar       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char    *read_from_file(int fd)
+{
+    char    *buffer;
+    size_t   bytes_read;
+
+    buffer = malloc((BUFFER_SIZE + 1) * sizeof (char));
+    bytes_read = read(fd, buffer, BUFFER_SIZE);
+    if (bytes_read <= 0)
+    {
+        free(buffer);
+        return (NULL);    
+    }
+    return (buffer);
+}
+
+char    *get_next_line(int fd)
+{
+    char    *line;
+    
+    line = read_from_file(fd);
+    return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letsaguiar <letsaguiar@yandex.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 10:01:40 by letsaguiar        #+#    #+#             */
+/*   Updated: 2024/04/30 10:10:24 by letsaguiar       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+char    *get_next_line(int fd);
+
+#endif
